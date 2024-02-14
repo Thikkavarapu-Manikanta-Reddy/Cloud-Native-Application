5d0bf855640228c7ef91e6c7e525db74290bcc6c
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2024-02-14 18:27:52 -0500

    Optional values

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ DB_DIALECT = 'mysql';
 
 module.exports = {
     development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOSTNAME,
+        username: process.env.DB_USER || 'root' ,
+        password: process.env.DB_PASSWORD || 'Godofwar@321',
+        database: process.env.DB_NAME || 'sys',
+        host: process.env.DB_HOSTNAME || 'localhost',
         dialect: DB_DIALECT,
     },
     test: {
