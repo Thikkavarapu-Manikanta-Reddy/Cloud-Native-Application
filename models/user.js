06626a1e37e651291955396173a665a5decb2063
Thikkavarapu Manikanta Reddy <t.manikantareddy160@gmail.com>
2024-03-31 12:25:16 -0400

    User emailVerified attribute added

--- models/user.js
+++ models/user.js
@@ -1,7 +1,6 @@
-'use strict';
+"use strict";
 
-
-const {Model} = require('sequelize');
+const { Model } = require("sequelize");
 
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
@@ -14,58 +13,64 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-    },
-    first_name: {
-      type: DataTypes.STRING,
-      required: true,
-      allowNull: false,
-    },
-    last_name: {
-      type: DataTypes.STRING,
-      required: true,
-      allowNull: false,
-    },
-    username: {
-      type: DataTypes.STRING,
-      required: true,
-      unique: {
-        msg: 'Username should be unique',
-        fields: ['username'],
+  User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
       },
-      allowNull: false,
-      validate: {
-        isEmail: {
-          args: true,
-          msg: 'User name should be a valid email address!',
+      first_name: {
+        type: DataTypes.STRING,
+        required: true,
+        allowNull: false,
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        required: true,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        required: true,
+        unique: {
+          msg: "Username should be unique",
+          fields: ["username"],
+        },
+        allowNull: false,
+        validate: {
+          isEmail: {
+            args: true,
+            msg: "User name should be a valid email address!",
+          },
         },
       },
+      password: {
+        type: DataTypes.STRING,
+        required: true,
+      },
+      emailVerified: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false,
+      },
+      verifyCode: {
+        type: DataTypes.STRING,
+      },
     },
-    password: {
-      type: DataTypes.STRING,
-      required: true,
-    },
-    verifyCode: {
-      type: DataTypes.STRING
+    {
+      sequelize,
+      modelName: "User",
+      timestamps: true,
+      createdAt: "account_created",
+      updatedAt: "account_updated",
     }
-  }, {
-    sequelize,
-    modelName: 'User',
-    timestamps: true,
-    createdAt: 'account_created',
-    updatedAt: 'account_updated',
-  }
   );
   // User.addHook('beforeCreate', (user, options) => {
   //   const salt = await bcrypt.genSalt(10);
   //   const hashedPassword = await bcrypt.hash( req.body.password , salt)
   //   user.password = hashPassword(user.password);
   // });
-  
+
   // User.addHook('beforeUpdate', (user, options) => {
   //   const salt = await bcrypt.genSalt(10);
   //   const hashedPassword = await bcrypt.hash( req.body.password , salt)
@@ -74,4 +79,3 @@ module.exports = (sequelize, DataTypes) => {
 
   return User;
 };
-
